acdb01382aec397d744a0d524e3dc4eb529921e1
ivanap14 <pejovic.ivana14@gmail.com>
2026-08-15 23:18:44 +0200

    Dodate klase za komunikaciju izmedju klijenta i servera

---
+++ ambulanta-common/src/main/java/rs/ac/bg/fon/ambulanta/communication/Operation.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package rs.ac.bg.fon.ambulanta.communication;
+
+/**
+ *
+ * @author Korisnik
+ */
+public enum Operation {
+    LOGIN,
+    GET_ALL_VETERINARIANS,
+    GET_ALL_ANIMALS,
+    GET_ALL_SERVICES,
+    ADD_NEW_INTERVENTION,
+    EDIT_INTERVENTION,
+    GET_INTERVENTIONS_BY_INTERVENTION_CRITERIA,
+    GET_INTERVENTIONS_BY_VETERINARIAN_CRITERIA,
+    GET_INTERVENTIONS_BY_ANIMAL_CRITERIA,
+    GET_INTERVENTIONS_BY_SERVICE_CRITERIA,
+    GET_ALL_INTERVENTIONS,
+    GET_INTERVENTION,
+    GET_ALL_OWNERS,
+    ADD_NEW_ANIMAL,
+    EDIT_ANIMAL,
+    GET_ANIMAL_BY_ANIMAL_CRITERIA,
+    GET_ANIMAL_BY_OWNER_CRITERIA,
+    GET_ANIMAL,
+    DELETE_ANIMAL,
+    ADD_NEW_SPECIALIZATION,
+    ADD_NEW_OWNER,
+    EDIT_OWNER
+}
---
+++ ambulanta-common/src/main/java/rs/ac/bg/fon/ambulanta/communication/Receiver.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.ambulanta.communication;
+
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class Receiver {
+    private Socket socket;
+
+    public Receiver(Socket socket) {
+        this.socket = socket;
+    }
+    
+    public Object receive() throws Exception{
+        try {
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+            return in.readObject();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw  new Exception("Greska u prijemu objekta: " + ex.getMessage());
+        }
+       
+    }
+}
