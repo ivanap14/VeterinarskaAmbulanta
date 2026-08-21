30f28aef1010cf6fba3cc044540841f8392aa583
ivanap14 <pejovic.ivana14@gmail.com>
2026-08-21 16:10:57 +0200

    Dokumentovane klase EditAnimal i EditIntervention

--- ambulanta-server/src/main/java/rs/ac/bg/fon/sa/ambulanta/operation/animal/EditAnimal.java
+++ ambulanta-server/src/main/java/rs/ac/bg/fon/sa/ambulanta/operation/animal/EditAnimal.java
@@ -11,36 +11,75 @@ import java.time.LocalDate;
 import java.util.List;
 
 /**
+ * Klasa zadužena za izmenu podataka o postojećoj životinji u bazi podataka.
+ *
+ * Nasleđuje apstraktnu klasu AbstractSO i implementira metode
+ * preconditions(Object) i executeOperation(Object),
+ * definišući na taj način sopstvena specifična poslovna pravila i logiku.
+ * Pre same izmene proverava da li životinja sa prosleđenim identifikatorom
+ * zaista postoji u bazi podataka.
  *
  * @author Korisnik
+ * @version 1.0
  */
 public class EditAnimal extends AbstractSO{
 
+	/**
+    * Životinja sa izmenjenim podacima nakon uspešnog izvršavanja operacije.
+    */
     private Animal animal;
     
+    /**
+     * Proverava da li je prosleđen parametar, da li predstavlja instancu
+     * klase Animal i da li životinja sa datim identifikatorom postoji u bazi
+     * podataka.
+     *
+     * @param param Životinja sa izmenjenim podacima. Ne sme biti null.
+     * @throws Exception Ako je parametar null, nije instanca klase Animal
+     * ili životinja sa prosleđenim identifikatorom ne postoji u bazi podataka.
+     */
     @Override
     protected void preconditions(Object param) throws Exception {
         if (param == null || !(param instanceof Animal)) {
             throw new Exception("Objekat pogresnog tipa.");
         }
         
-        animal = (Animal) param;
+        Animal animal1 = (Animal) param;
 
-        checkExist(animal);
+        checkExist(animal1);
         
 
     }
 
+    /**
+     * Menja podatke prosleđene životinje u bazi podataka i čuva je kao
+     * rezultat operacije.
+     *
+     * @param param Životinja sa izmenjenim podacima.
+     * @throws Exception Ako izmena životinje u bazi podataka ne uspe.
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
         animal = (Animal) repository.edit((Animal)param);
     }
 
+    /**
+     * Vraća životinju sa izmenjenim podacima nakon izvršavanja operacije.
+     *
+     * @return izmenjena životinja
+     */
     public Animal getAnimal() {
         return animal;
     }
 
-    
+    /**
+     * Proverava da li životinja sa identifikatorom prosleđene životinje
+     * postoji u bazi podataka.
+     *
+     * @param animal Životinja čije se postojanje proverava.
+     * @throws Exception Ako životinja sa datim identifikatorom ne postoji
+     * u bazi podataka.
+     */
     private void checkExist(Animal animal) throws Exception {
         List<Animal> list = repository.getByCriteria(new Animal(),"WHERE a.id=" + animal.getId());
 
--- ambulanta-server/src/main/java/rs/ac/bg/fon/sa/ambulanta/operation/intervention/EditIntervention.java
+++ ambulanta-server/src/main/java/rs/ac/bg/fon/sa/ambulanta/operation/intervention/EditIntervention.java
@@ -9,13 +9,38 @@ import java.util.List;
 import rs.ac.bg.fon.sa.ambulanta.operation.*;
 
 /**
+ * Klasa zadužena za izmenu podataka o postojećoj intervenciji
+ * u bazi podataka.
+ *
+ * Nasleđuje apstraktnu klasu AbstractSO i implementira metode
+ * preconditions(Object) i executeOperation(Object),
+ * definišući na taj način sopstvena specifična poslovna pravila i logiku.
+ * Pre same izmene proverava da li intervencija sa prosleđenim identifikatorom
+ * postoji u bazi podataka i da li poseduje bar jednu stavku (uslugu). Prilikom
+ * izvršavanja operacije, postojeće stavke intervencije se brišu i zamenjuju
+ * novoprosleđenim stavkama.
  *
  * @author Korisnik
+ * @version 1.0
  */
 public class EditIntervention extends AbstractSO{
 
+	/**
+     * Intervencija nad kojom se vrši izmena, odnosno intervencija sa
+     * izmenjenim podacima nakon uspešnog izvršavanja operacije.
+     */
     private Intervention intervention;
     
+    /**
+     * Proverava da li je prosleđen parametar, da li predstavlja instancu
+     * klase Intervention, da li intervencija sa datim identifikatorom
+     * postoji u bazi podataka i da li poseduje bar jednu stavku.
+     *
+     * @param param Intervencija sa izmenjenim podacima. Ne sme biti null.
+     * @throws Exception Ako je parametar null, nije instanca klase Intervention,
+     * intervencija sa prosleđenim identifikatorom ne postoji u bazi podataka
+     * ili ne sadrži nijednu uslugu.
+     */
     @Override
     protected void preconditions(Object param) throws Exception {
         if (param == null || !(param instanceof Intervention)) {
@@ -29,6 +54,15 @@ public class EditIntervention extends AbstractSO{
         checkOtherConstraints(intervention);
     }
 
+    /**
+     * Briše postojeće stavke prosleđene intervencije, menja samu
+     * intervenciju u bazi podataka, a zatim ponovo dodaje njene stavke
+     * kako bi bile ažurne.
+     *
+     * @param param Intervencija sa izmenjenim podacima i stavkama.
+     * @throws Exception Ako brisanje starih stavki, izmena intervencije ili
+     * dodavanje novih stavki u bazu podataka ne uspe.
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
          
@@ -48,10 +82,23 @@ public class EditIntervention extends AbstractSO{
         }
     }
 
+    /**
+     * Vraća intervenciju sa izmenjenim podacima nakon izvršavanja operacije.
+     *
+     * @return izmenjena intervencija
+     */
     public Intervention getIntervention() {
         return intervention;
     }
     
+    /**
+     * Proverava da li intervencija sa identifikatorom prosleđene intervencije
+     * postoji u bazi podataka.
+     *
+     * @param intervention Intervencija čije se postojanje proverava.
+     * @throws Exception Ako intervencija sa datim identifikatorom ne postoji
+     * u bazi podataka.
+     */
     private void checkExist(Intervention intervention) throws Exception {
         List<Intervention> list = repository.getByCriteria(new Intervention(),"WHERE i.id=" + intervention.getId());
 
@@ -60,7 +107,13 @@ public class EditIntervention extends AbstractSO{
         }
     }
     
-
+    /**
+     * Proverava da li prosleđena intervencija sadrži bar jednu stavku (uslugu).
+     *
+     * @param intervention1 Intervencija čije se stavke proveravaju. Nije
+     * korišćen, provera se vrši nad poljem klase.
+     * @throws Exception Ako intervencija ne sadrži nijednu uslugu.
+     */
     private void checkOtherConstraints(Intervention intervention1) throws Exception {
         if (intervention.getInterventionItems().isEmpty()) {
             throw new Exception("Intervencija mora sadržati najmanje jednu uslugu.");
